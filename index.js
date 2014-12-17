64c717060bffc8fc55ea96cfb55125911d60c600
hochitom <hochitom@icloud.com>
2014-12-17 18:23:24 +0100

    added tumblr

--- index.js
+++ index.js
@@ -9,15 +9,17 @@ var get = q.nbind(request, get);
 
 http.createServer(function (req, res) {
     // @TODO: add twitter, instagram
-    q.all([
+    q.allSettled([
         get('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=hochitom&format=json&api_key=' + config.keys.lastfm),
+        get('http://api.tumblr.com/v2/blog/hlbh.tumblr.com/posts?api_key=' + config.keys.tumblr)
     ])
-    .spread(function (lastfm, twitter) {
-        lastfm = lastfm[0];
+    .spread(function (lastfm, tumblr) {
+        lastfm = lastfm.value[0].body;
+        tumblr = tumblr.value[0].body;
 
         // @TODO: normalize data
 
-        res.end(lastfm);
+        res.end(tumblr);
     })
     .fail(function (err) {
         console.error(err);
