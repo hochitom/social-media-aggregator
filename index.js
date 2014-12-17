1233755ab8660265fda3588d8f828dcb6e4b523a
hochitom <hochitom@icloud.com>
2014-12-17 17:51:24 +0100

    created config file and sample

--- index.js
+++ index.js
@@ -3,13 +3,14 @@
 var http = require('http');
 var q = require('q');
 var request = require('request');
+var config = require('./config');
 
 var get = q.nbind(request, get);
 
 http.createServer(function (req, res) {
     // @TODO: add twitter, instagram
     q.all([
-        get('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=hochitom&api_key=7473c4a6ba5e95d828c00f1a0089a3f1&format=json'),
+        get('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=hochitom&format=json&api_key=' + config.keys.lastfm),
     ])
     .spread(function (lastfm, twitter) {
         lastfm = lastfm[0];
